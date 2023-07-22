c594877ff685321559ab3219a20c8a0b2c477d6c
Pranav Hundekari <@PranavHun>
2023-07-22 17:48:07 +0530

    Key Presses

--- blankogl.py
+++ blankogl.py
@@ -6,6 +6,31 @@ import pyrr
 from PIL import Image
 import time 
 
+xmov = 0.0
+ymov = 0.0
+dxmov = 0.05
+rotation = 0
+
+
+def key_callback_func(window, key, scancode, action, mods):
+    global xmov, dxmov, ymov, rotation
+    # print(f"{key} {action}")
+
+    if key == glfw.KEY_CAPS_LOCK and action == glfw.PRESS:
+        glfw.set_window_should_close(window, GL_TRUE)
+    if key == glfw.KEY_A and (action == glfw.PRESS or action == glfw.REPEAT):
+        xmov -= dxmov
+    if key == glfw.KEY_D and (action == glfw.PRESS or action == glfw.REPEAT):
+        xmov += dxmov
+    if key == glfw.KEY_W and (action == glfw.PRESS or action == glfw.REPEAT):
+        ymov += dxmov
+    if key == glfw.KEY_S and (action == glfw.PRESS or action == glfw.REPEAT):
+        ymov -= dxmov
+    if key == glfw.KEY_LEFT and (action == glfw.PRESS or action == glfw.REPEAT):
+        rotation = rotation - 1 if rotation > -35 else -35
+    if key == glfw.KEY_RIGHT and (action == glfw.PRESS or action == glfw.REPEAT):
+        rotation = rotation + 1 if rotation < 35 else 35
+
 def create_transform_mat (translate_x, translate_y, translate_z, rotate_x, rotate_y, rotate_z, scale_x, scale_y, scale_z):
     transform_mat = pyrr.Matrix44.identity()
     transform_mat = transform_mat * pyrr.matrix44.create_from_translation(np.array([translate_x, translate_y, translate_z]))
@@ -26,61 +51,19 @@ def main():
         return
 
     glfw.make_context_current(window)
-
+    
     cube = [-0.5, -0.5, 0.0,
              0.5, -0.5, 0.0,
              0.0, 0.5, 0.0
         ]
-    
-
-
 # convert to 32bit float
 
     cube = np.array(cube, dtype=np.float32)
-
     indices = [0, 1, 2]
-
     indices = np.array(indices, dtype=np.uint32)
 
-    VERTEX_SHADER = """
-
-              #version 440
-
-              layout (location = 0) in vec3 position;
-
-              out vec3 newColor;
-              
-              uniform mat4 transform; 
-
-              void main() {
-
-                vec4 pos = transform * vec4(position, 1.0f);
-
-               gl_Position = pos;
-               newColor = vec3(1.0f, 1.0f, 1.0f) / abs(pos.z * pos.z);
-              }
-
-
-
-          """
-
-    FRAGMENT_SHADER = """
-           #version 330
-
-            in vec3 newColor;
-
-            out vec4 outColor;
-
-           void main() {
-              outColor = vec4(newColor, 1.0);
-            }
-
-       """
-
-
-
-    # Compile The Program and shaders
-
+    VERTEX_SHADER = open("shader.vert").read()
+    FRAGMENT_SHADER = open("shader.frag").read()
     shader = OpenGL.GL.shaders.compileProgram(OpenGL.GL.shaders.compileShader(VERTEX_SHADER, GL_VERTEX_SHADER),
                                               OpenGL.GL.shaders.compileShader(FRAGMENT_SHADER, GL_FRAGMENT_SHADER))
 
@@ -107,19 +90,18 @@ def main():
     glEnable(GL_DEPTH_TEST)
 
     projection = pyrr.Matrix44.identity() * pyrr.matrix44.create_perspective_projection(45.0, 1920.0 / 1080.0, 0.1, 100.0)
-    view_matrix = pyrr.Matrix44.identity() * pyrr.matrix44.create_look_at(
-        pyrr.vector3.create(0, 0.4, 1), #camera position
+    view_matrix = pyrr.matrix44.create_look_at(
+        pyrr.vector3.create(0, 0.0, 1), #camera position
         pyrr.vector3.create(0, 0, 0), #camera target
         pyrr.vector3.create(0, 1, 0)  #camera up vector
         )
-    perspective = projection * view_matrix
-    print(perspective * create_transform_mat(0.5, 0.0, 0.0, 45, 0 , 0, 0.5, 0.5, 0.5))
-    rotation = 0
-    xmov = 0.0
-    dxmov = 0.05
+    
+    ypos = ((np.random.random(100) - 0.5) / 3.0)
 
     WAIT_TIME = 1000.0 / 60.0
     frame_start = time.time()
+
+    glfw.set_key_callback(window, key_callback_func)
     
     while not glfw.window_should_close(window):
         glfw.poll_events()
@@ -129,26 +111,23 @@ def main():
         transformLoc = glGetUniformLocation(shader, "transform")
 
         for i in range(100):
-            transform_mat = create_transform_mat(-0.5+xmov+dxmov, 0.0, -i, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5)
-            # transform_mat_global = create_transform_mat(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0)
-            glUniformMatrix4fv(transformLoc, 1, GL_FALSE, perspective * transform_mat)
+            camera_transform_mat = create_transform_mat(0.0, 0.0, 0.0, 0.0, rotation, 0.0, 1.0, 1.0, 1.0)
+            perspective = projection * camera_transform_mat * view_matrix
+            transform_mat = create_transform_mat(-0.5+xmov+dxmov, ypos[i]+ymov, -i, 0.0, i*45.0/100, 0.0, 0.5, 0.5, 0.5)
+            transform_mat_global = create_transform_mat(0.0, 0.0, 0.0, 0.0, i*45.0/100.0, 0.0, 1.0, 1.0, 1.0)
+            glUniformMatrix4fv(transformLoc, 1, GL_FALSE, perspective * transform_mat_global * transform_mat)
             glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, None)
 
-            transform_mat = create_transform_mat(0.5+xmov+dxmov, 0.0, -i, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5)
-            # transform_mat_global = create_transform_mat(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0)
-            glUniformMatrix4fv(transformLoc, 1, GL_FALSE, perspective * transform_mat)
+            transform_mat = create_transform_mat(0.5+xmov+dxmov, ypos[i]+ymov, -i, 0.0, i*45.0/100, 0.0, 0.5, 0.5, 0.5)
+            transform_mat_global = create_transform_mat(0.0, 0.0, 0.0, 0.0, i*45.0/100.0, 0.0, 1.0, 1.0, 1.0)
+            glUniformMatrix4fv(transformLoc, 1, GL_FALSE, perspective * transform_mat_global * transform_mat)
             glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, None)
-
-        rotation = 0 if rotation == 360 else rotation + 1
-        dxmov = -dxmov if xmov >= 1.0 or xmov <= -1.0 else dxmov
-        xmov += dxmov
 
         glfw.swap_buffers(window)
         if(time.time() - frame_start) < WAIT_TIME:
             duration = (WAIT_TIME - (time.time() - frame_start))
             time.sleep(duration / 1000.0)
         frame_start = time.time()
-
 
     glfw.terminate()
 
---
+++ shader.vert
@@ -0,0 +1,11 @@
+#version 440
+
+layout (location = 0) in vec3 position;
+out vec3 newColor;
+uniform mat4 transform; 
+
+void main() {
+    vec4 pos = transform * vec4(position, 1.0f);
+    gl_Position = pos;
+    newColor = vec3(1.0f, 1.0f, 1.0f) / abs(pos.z);
+}
